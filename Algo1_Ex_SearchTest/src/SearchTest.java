498fc09566efbbaa3ce1e9d79ab45a551a9751e0
Aviral Rajput <aviral282@gmail.com>
2019-01-23 09:34:26 +0000

    Algo Example 1
    Linear Search

---
+++ Algo1_Ex_SearchTest/src/SearchTest.java
@@ -0,0 +1,32 @@
+import java.awt.Graphics;
+import java.util.Date;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Scanner;
+ 
+
+public class SearchTest {
+
+public static boolean search(int[] list, int target){
+	boolean res= false;
+	for(int i = 0;i<list.length; i++) {
+		if(list[i] == target)
+			res = true;
+	}
+	
+	return res;
+}
+
+public static void main(String[] args)
+{
+			int[] list={4,1,3,8,5,2,11,21,6};
+                        Scanner sc = new Scanner(System.in);
+
+			System.out.print("Enter Target: ");
+			int target =sc.nextInt();
+
+			boolean res=search(list,target);
+			System.out.println("Target Found: "+res);
+			}
+}
