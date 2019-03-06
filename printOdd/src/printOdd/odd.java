8f7729397ca7bcf06350c34f48f0445e535264ce
Aviral Rajput <aviral282@gmail.com>
2019-03-06 10:51:13 +0000

    odd

---
+++ printOdd/src/printOdd/odd.java
@@ -0,0 +1,17 @@
+package printOdd;
+
+public class odd {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int x=0; x<=100; x++)
+			{
+			if( x%2==0) {
+				System.out.println("value of x :" + x);
+			}
+			}
+	}
+
+}
