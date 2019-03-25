5540f7d0aafbfa8c4ac601143d0ee7e4cdcbcc2a
Aviral Rajput <aviral282@gmail.com>
2019-03-25 10:10:44 +0000

    Recursion Rivision

---
+++ Recursion Rivision Q_1/src/TestRecursionQ1.java
@@ -0,0 +1,6 @@
+
+public class TestRecursionQ1 {
+public static void main(String[] args){
+	MyFrameRecursionQ1 mf2=new MyFrameRecursionQ1("First");
+}
+}
---
+++ Recursion Rivision Q_2/src/TestRecursionQ2.java
@@ -0,0 +1,6 @@
+
+public class TestRecursionQ2 {
+	public static void main(String[] args) {
+		MyFrameRecursionQ2 mf2 = new MyFrameRecursionQ2("First");
+	}
+}
